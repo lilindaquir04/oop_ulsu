813f8e42713db3c88ca61f08b067c833eea23e11
Marat Bulatov <bulatoffmaratka@yandex.ru>
2025-09-25 12:11:07 +0400

    Add files via upload

---
+++ ООПЛаба8/ООПЛаба8/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // === а–д: работа с long (встроенный тип) ===
+        var longList = LongListProcessor.CreateList();
+        LongListProcessor.PrintList(longList, "а. Начальный список long:");
+
+        LongListProcessor.ModifyList(longList);
+        LongListProcessor.PrintList(longList, "б. После удаления кратных 20 и замены 30 на 35:");
+
+        Console.WriteLine("в. Просмотр с итераторами long:");
+        foreach (var item in longList)
+            Console.Write(item + " ");
+        Console.WriteLine("\n");
+
+        LongListProcessor.RemoveAfterIndex(longList, 2, 2);
+        LongListProcessor.PrintList(longList, "г. После удаления 2 элементов после индекса 2:");
+
+        LongListProcessor.SortDescending(longList);
+        LongListProcessor.PrintList(longList, "д. Отсортированный список long по убыванию:");
+
+        // === е–ж: работа с Int (пользовательский тип) ===
+        var intList = IntListProcessor.CreateList();
+        IntListProcessor.PrintList(intList, "е. Начальный список Int:");
+
+        IntListProcessor.ModifyList(intList);
+        IntListProcessor.PrintList(intList, "   После удаления кратных 20 и замены 30 на 35:");
+
+        Console.WriteLine("   Просмотр с итераторами Int:");
+        foreach (var item in intList)
+            Console.Write(item + " ");
+        Console.WriteLine("\n");
+
+        IntListProcessor.RemoveAfterIndex(intList, 2, 2);
+        IntListProcessor.PrintList(intList, "   После удаления 2 элементов после индекса 2:");
+
+        IntListProcessor.SortDescending(intList);
+        IntListProcessor.PrintList(intList, "   Отсортированный список Int по убыванию:");
+
+        // ж. Поиск элемента
+        var searchElement = new Int(35);
+        var found = IntListProcessor.FindElement(intList, searchElement);
+        if (found != null)
+            Console.WriteLine($"ж. Найден элемент: {found}");
+        else
+            Console.WriteLine("ж. Элемент не найден");
+
+        // з. Подсчёт элементов > 30
+        int countLong = longList.Count(x => x > 30);
+        Console.WriteLine($"з. Количество элементов > 30 в longList: {countLong}");
+
+        int countInt = intList.Count(x => x.GetValue() > 30);
+        Console.WriteLine($"   Количество элементов с value > 30 в intList: {countInt}");
+    }
+}
---
+++ ООПЛаба8/ООПЛаба8/intlist.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class IntListProcessor
+{
+    public static LinkedList<Int> CreateList()
+    {
+        var list = new LinkedList<Int>();
+        foreach (var x in new int[] { 10, 20, 30, 40, 50, 60, 70 })
+            list.AddLast(new Int(x));
+        return list;
+    }
+
+    public static void ModifyList(LinkedList<Int> list)
+    {
+        // Удалить кратные 20
+        var node = list.First;
+        while (node != null)
+        {
+            var next = node.Next;
+            if (node.Value.GetValue() % 20 == 0)
+                list.Remove(node);
+            node = next;
+        }
+
+        // Заменить 30 на 35
+        node = list.First;
+        while (node != null)
+        {
+            if (node.Value.GetValue() == 30)
+                node.Value = new Int(35);
+            node = node.Next;
+        }
+    }
+
+    public static void RemoveAfterIndex(LinkedList<Int> list, int index, int n)
+    {
+        var node = list.First;
+        for (int i = 0; i < index && node != null; i++)
+            node = node.Next;
+
+        if (node == null) return;
+
+        for (int i = 0; i < n && node.Next != null;)
+        {
+            var toRemove = node.Next;
+            list.Remove(toRemove);
+            i++;
+        }
+    }
+
+    public static void SortDescending(LinkedList<Int> list)
+    {
+        var sorted = list.OrderByDescending(x => x.GetValue()).ToList();
+        list.Clear();
+        foreach (var item in sorted)
+            list.AddLast(item);
+    }
+
+    public static void PrintList(LinkedList<Int> list, string message)
+    {
+        Console.WriteLine(message);
+        foreach (var item in list)
+            Console.Write(item + " ");
+        Console.WriteLine("\n");
+    }
+
+    public static Int? FindElement(LinkedList<Int> list, Int element)
+    {
+        return list.FirstOrDefault(x => x.Equals(element));
+    }
+}
